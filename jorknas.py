60efc73d08613999852ecbe7779d74e849169951
Ben Meredith <ben.meredith1@icloud.com>
2025-08-20 22:37:03 -0400

    Update jorknas.py

--- jorknas.py
+++ jorknas.py
@@ -384,5 +384,5 @@ def view_messages(username):
 # ----------------------------
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
